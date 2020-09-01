09f1b23f491900bfd67ad8bd1a2abfdfa82f2884
Choi Jun Chan <bada8908@gmail.com>
2020-09-02 00:17:00 +0900

    Initial Commit

---
+++ trainingWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainingWeatherApp
+//
+//  Created by 최준찬 on 2020/09/02.
+//  Copyright © 2020 최준찬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
