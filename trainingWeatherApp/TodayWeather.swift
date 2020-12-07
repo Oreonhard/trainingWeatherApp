f9a7f81b0b39f1fdc446d57b48d278e0bd2f5c66
Choi Jun Chan <bada8908@gmail.com>
2020-12-07 23:39:12 +0900

    - 오늘의 날씨 View 	(1) 위치 권한 관련 분기 작업

--- trainingWeatherApp/TodayWeather.swift
+++ trainingWeatherApp/TodayWeather.swift
@@ -10,8 +10,12 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class TodayWeather: UIViewController, CLLocationManagerDelegate {
-
+class TodayWeather: UIViewController {
+    
+    //MARK: UI Variable
+    @IBOutlet weak var AreaName: UILabel!
+    
+    //MARK: Global Variable
     let locationManager = CLLocationManager();
     
     override func viewDidLoad() {
@@ -21,17 +25,51 @@ class TodayWeather: UIViewController, CLLocationManagerDelegate {
         self.locationManager.requestAlwaysAuthorization()
     }
     
+    
+    @IBAction func reloadLocation(_ sender: Any) {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            self.locationManager.requestLocation()
+        case .denied:
+            let alertCon = UIAlertController(title:"trainingWeatherApp", message: "위치 권한이 거부되어 있습니다.\n 앱 설정에서 권한을 허용해주세요.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "확인", style: .default, handler: { _ in
+                if #available(iOS 13, *){
+                    if let settingUrl = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(settingUrl){
+                        UIApplication.shared.open(settingUrl, completionHandler: {
+                            success in
+                            print("Open Setting : \(success)")
+                        })
+                    }
+                }
+            })
+            alertCon.addAction(alertAction)
+            self.present(alertCon, animated: true, completion: nil)
+        default:
+            self.locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+}
+
+extension TodayWeather : CLLocationManagerDelegate {
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locationValue : CLLocationCoordinate2D = manager.location?.coordinate else { return }
         print("Locatoin Coordinate : \(locationValue.longitude) \(locationValue.latitude)")
     }
     
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        // iOS 14 이상부터 지원 가능 > 현재 개발 중인 앱 지원 범위는 iOS 12 이상
-        print("locationManagerDidChangeAuthorization Work")
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            self.locationManager.requestLocation()
+        default:
+            AreaName.text = "위치 조회에 실패하였습니다."
+        }
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-       print(status)
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("locationManager did Fail : \(error.localizedDescription)")
     }
 }
