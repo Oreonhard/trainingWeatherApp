12af99cfb9d9765ac29f42036cc2f9fd14e56306
Choi Jun Chan <bada8908@gmail.com>
2021-04-09 00:53:59 +0900

    지역별 날씨 View UI 작업
    추후 작업 : SearchBar 관련해서 추가로 검색 필요

--- trainingWeatherApp/RegionWeatherTableViewController.swift
+++ trainingWeatherApp/RegionWeatherTableViewController.swift
@@ -12,9 +12,22 @@ class RegionWeatherTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "regionWeatherCell")!
+    }
 }
 
